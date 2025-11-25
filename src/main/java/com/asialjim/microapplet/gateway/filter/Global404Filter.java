3a49d047b15fd892c122528a23bbb23d6e3a7c02
Asial Jim <asialjim@qq.com>
2025-11-25 08:39:12 +0000

    ```bash git commit -m "Remove commented Nacos Config dependency and unused imports" ```
    This commit message is concise, clear, and captures the main changes made. It uses imperative mood ("Remove") and present tense. The phrase "commented Nacos Config dependency" explains that the dependency was commented out in the pom.xml file, while "unused imports" covers removing unnecessary Java imports from various files.
    
    Signed-off-by: Asial Jim <asialjim@qq.com>

--- src/main/java/com/asialjim/microapplet/gateway/filter/Global404Filter.java
+++ src/main/java/com/asialjim/microapplet/gateway/filter/Global404Filter.java
@@ -23,11 +23,9 @@ import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpResponse;
-import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
