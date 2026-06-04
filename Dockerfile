3ebb7fff515a59bc774d1e2e9493ab81023cef5e
Asial Jim <asialjim@qq.com>
2026-06-04 18:48:24 +0800

    update

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
 #指定基础镜像
-FROM openjdk:8u121-jre-alpine
+FROM jre:25
 EXPOSE 80
 COPY ./*.jar /
 # 赋予执行权限
-RUN chmod 755 -R /Application.jar
+RUN chmod 755 -R /app.jar
 # 设置
-ENTRYPOINT ["java","-Xms256m", "-Xmx1g", "-jar", "/Application.jar"]
+ENTRYPOINT ["java","-Xms256m", "-Xmx1g", "-jar", "/app.jar"]
 MAINTAINER AsialJim
--- docker.sh
+++ docker.sh
@@ -16,9 +16,9 @@
 #
 
 # 定义应用组名
-group_name='asialjim'
+group_name='aj-mams'
 # 定义应用名称 ,这里的name是获取你仓库的名称，也可以自己写
-app_name='mams-gateway'
+app_name='gateway'
 docker_name='aj-mams-gateway'
 # 定义应用版本
 app_version='latest'
@@ -35,14 +35,14 @@ docker rmi ${group_name}/${app_name}:${app_version}
 docker build -t ${group_name}/${app_name}:${app_version} .
 
 docker run --name ${docker_name} \
---network api \
---ip 172.100.0.100 \
+--network service \
+--ip 172.20.96.210 \
 --cpus="2" --memory="1g" \
 --env-file /root/.env/mams.env \
 -e 'spring.profiles.active'=${profile_active} \
 -e TZ="Asia/Shanghai" \
 -v /etc/localtime:/etc/localtime \
--v /home/asialjim/.app/docker/github/MicroBank/${app_name}/logs:/var/logs \
+-v /home/mams/logs/${group_name}/${app_name}:/var/logs \
 -d ${group_name}/${app_name}:${app_version}
 
 
